01c6675578335abcc80e8bbcb227c4f5f0b03408
obrunofontana <obrunofontana@gmail.com>
2023-02-03 19:15:52 -0300

    feat: adiciona paginas para tratamento de erros

---
+++ src/pages/ErrorPages/PageUnauthorized/PageUnauthorized.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const PageUnauthorized: React.FC = () => {
+  return <div>Página não autorizada</div>;
+};
+
+export default PageUnauthorized;
---
+++ src/pages/ErrorPages/PageUnexpected/PageUnexpected.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const PageUnexpected: React.FC = () => {
+  return <div>Desculpe, algo deu errado:( </div>;
+};
+
+export default PageUnexpected;
