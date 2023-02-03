f571be86eff3a0f072eefeb62480f0d474a59944
obrunofontana <obrunofontana@gmail.com>
2023-02-03 19:30:33 -0300

    feat: implementa reducer de autenticação

---
+++ src/store/api/authApi.ts
@@ -0,0 +1,66 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+import { RegisterInput } from 'pages/RegisterPage/RegisterPage';
+import { LoginInput } from 'pages/SignInPage/SignInPage';
+
+import { IGenericResponse } from './types';
+import { userApi } from './userApi';
+
+const BASE_URL = import.meta.env.VITE_SERVER_ENDPOINT as string;
+
+export const authApi = createApi({
+  reducerPath: 'authApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${BASE_URL}/api/auth/`,
+  }),
+  endpoints: (builder) => ({
+    registerUser: builder.mutation<IGenericResponse, RegisterInput>({
+      query(data) {
+        return {
+          url: 'register',
+          method: 'POST',
+          body: data,
+        };
+      },
+    }),
+    loginUser: builder.mutation<{ access_token: string; status: string }, LoginInput>({
+      query(data) {
+        return {
+          url: 'login',
+          method: 'POST',
+          body: data,
+          credentials: 'include',
+        };
+      },
+      async onQueryStarted(args, { dispatch, queryFulfilled }) {
+        try {
+          await queryFulfilled;
+          await dispatch(userApi.endpoints.getMe.initiate(null));
+        } catch (error) {}
+      },
+    }),
+    verifyEmail: builder.mutation<IGenericResponse, { verificationCode: string }>({
+      query({ verificationCode }) {
+        return {
+          url: `verifyemail/${verificationCode}`,
+          method: 'GET',
+        };
+      },
+    }),
+    logoutUser: builder.mutation<void, void>({
+      query() {
+        return {
+          url: 'logout',
+          credentials: 'include',
+        };
+      },
+    }),
+  }),
+});
+
+export const {
+  useLoginUserMutation,
+  useRegisterUserMutation,
+  useLogoutUserMutation,
+  useVerifyEmailMutation,
+} = authApi;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,21 +1,22 @@
 import { configureStore, Action } from '@reduxjs/toolkit';
-import { useDispatch } from 'react-redux';
+import { useDispatch, TypedUseSelectorHook, useSelector } from 'react-redux';
 import logger from 'redux-logger';
 import { ThunkAction } from 'redux-thunk';
 
-const middleware = [logger];
-
+import { authApi } from './api/authApi';
+import { userApi } from './api/userApi';
 import rootReducer, { RootState } from './rootReducer';
 
 const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(middleware),
+  devTools: process.env.NODE_ENV === 'development',
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({}).concat([logger, authApi.middleware, userApi.middleware]),
 });
-
-// export type RootState = ReturnType<typeof store.getState>;
 
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = () => useDispatch();
 export type AppThunk = ThunkAction<void, RootState, unknown, Action>;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export default store;
