42ee086b931e383fecfe1ec3664695ba3dc3dfae
obrunofontana <obrunofontana@gmail.com>
2023-02-03 19:31:26 -0300

    feat: mapeia novos reducers

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -2,8 +2,15 @@ import { combineReducers } from '@reduxjs/toolkit';
 
 import appReducer from 'store/slices/AppSlice';
 
+import { authApi } from './api/authApi';
+import { userApi } from './api/userApi';
+import userReducer from './slices/UserSlice';
+
 const rootReducer = combineReducers({
   app: appReducer,
+  [authApi.reducerPath]: authApi.reducer,
+  [userApi.reducerPath]: userApi.reducer,
+  userState: userReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
