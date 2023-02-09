237c64d79c0f045d0721b53829ca0de6f7fa33b7
obrunofontana <obrunofontana@gmail.com>
2023-02-09 19:55:49 -0300

    feat: implementa scroll horizontal e scroll com a rodinha do mouse na horizontal

--- src/components/ProjectList/ProjectList.tsx
+++ src/components/ProjectList/ProjectList.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import Card from 'components/Card/Card';
 
-import { ProjectListContainer, ProjectListContainerContent, Separator } from './ProjectListStyles';
+import { ProjectListContainer, ProjectListContainerContent } from './ProjectListStyles';
 
 type ProjectListProps = {
   label: string;
@@ -10,12 +10,26 @@ type ProjectListProps = {
 };
 
 const ProjectList: React.FC<ProjectListProps> = ({ label, projects }) => {
+  const projectContainerContentRef = useRef<HTMLDivElement>();
+
+  useEffect(() => {
+    if (!projectContainerContentRef) return;
+
+    projectContainerContentRef.current?.addEventListener('wheel', (event) => {
+      event.preventDefault();
+
+      projectContainerContentRef.current?.scrollBy({
+        left: event.deltaY < 0 ? -30 : 30,
+      });
+    });
+  }, [projectContainerContentRef]);
+
   return (
     <ProjectListContainer>
       <label>{label}</label>
-      <ProjectListContainerContent>
-        {projects.map((project) => (
-          <Card project={project} />
+      <ProjectListContainerContent ref={projectContainerContentRef}>
+        {projects.map((project, index) => (
+          <Card project={project} key={index} />
         ))}
       </ProjectListContainerContent>
     </ProjectListContainer>
