c948c566f4ac6c36be028b2376d47cbce000c13b
obrunofontana <obrunofontana@gmail.com>
2023-02-11 17:43:48 -0300

    refactor: melhora tela 404

--- src/pages/ErrorPages/PageNotFound/PageNotFound.tsx
+++ src/pages/ErrorPages/PageNotFound/PageNotFound.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Box, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 const PageNotFound: React.FC = () => {
-  return <div>Página não encontrada</div>;
+  const navigate = useNavigate();
+
+  const navigateHomeHandler = () => {
+    navigate('/', { replace: true });
+  };
+
+  return (
+    <Box
+      height="100%"
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      <img src="404-2.png" alt="Not found image" width={500} />
+      <h1>Página não encontrada!</h1>
+      <Button onClick={navigateHomeHandler}>Voltar para home</Button>
+    </Box>
+  );
 };
 
 export default PageNotFound;
