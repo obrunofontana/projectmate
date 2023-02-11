eb4677ad5bf7d1f53a6cf4dc5b85fbe9389b2e77
obrunofontana <obrunofontana@gmail.com>
2023-02-11 09:37:44 -0300

    feat: adiciona um index para melhorar a importacao dos componentes

---
+++ src/components/index.ts
@@ -0,0 +1,14 @@
+export { default as AvatarMenu } from './AvatarMenu/AvatarMenu';
+export { default as Card } from './Card/Card';
+export { default as FormInput } from './FormInput/FormInput';
+export { default as FullScreenLoader } from './FullScreenLoader/FullScreenLoader';
+export { default as Layout } from './Layout/Layout';
+export { default as Modal } from './Modal/Modal';
+export { default as Navbar } from './Navbar/Navbar';
+export { default as ProjectList } from './ProjectList/ProjectList';
+export { default as SwitchTheme } from './SwitchTheme/SwitchTheme';
+export { default as ConfirmationModal } from './ConfirmationModal/ConfirmationModal';
+export type { ConfirmationModalProps } from './ConfirmationModal/ConfirmationModal';
+export { default as ApplicationContextContainer } from './ApplicationContextContainer/ApplicationContextContainer';
+export { default as TextAreaInput } from './TextAreaInput/TextAreaInput';
+export { default as Input } from './Input/Input';
