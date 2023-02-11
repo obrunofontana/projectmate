8413cace18179c7bad0a02023123ca37ffcdea59
obrunofontana <obrunofontana@gmail.com>
2023-02-11 17:43:20 -0300

    refactor: melhora a tela unexpected

--- src/pages/ErrorPages/PageUnexpected/PageUnexpected.tsx
+++ src/pages/ErrorPages/PageUnexpected/PageUnexpected.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
+import { Box } from '@mui/material';
+
 // import { Container } from './styles';
 
 const PageUnexpected: React.FC = () => {
-  return <div>Desculpe, algo deu errado:( </div>;
+  return (
+    <Box
+      height="100%"
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <h1>Desculpe, algo inesperado aconteceu :(</h1>
+    </Box>
+  );
 };
 
 export default PageUnexpected;
