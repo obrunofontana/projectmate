48ac0209885438cf5d9af6a5d7c3a29fecef946f
obrunofontana <obrunofontana@gmail.com>
2023-02-11 09:42:51 -0300

    refactor: mock do cadastro de projetos

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,17 @@
-import AddIcon from '@mui/icons-material/Add';
-import { Fab } from '@mui/material';
+import { useState } from 'react';
 
+import AddIcon from '@mui/icons-material/Add';
+import { Box, Button, Container, Fab } from '@mui/material';
+
+import { Modal, Input, TextAreaInput } from 'components';
 import ProjectList from 'components/ProjectList/ProjectList';
 
 import { HomeContainer } from './HomeStyles';
 
 const Home: React.FC = () => {
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  const [projectTitle, setProjectTitle] = useState<string>('');
+  const [projectDescription, setProjectDescription] = useState<string>('');
   const projectsViewRecently = ['Goiabão'];
   const myProjects = ['Avocado', 'Abacaxi'];
   const publicProjects = [
@@ -19,6 +25,44 @@ const Home: React.FC = () => {
     'Kiwi',
   ];
 
+  const clearFields = () => {
+    setProjectTitle('');
+    setProjectDescription('');
+  };
+
+  const onCloseModalHandler = () => {
+    setOpenModal(false);
+    clearFields();
+  };
+
+  const onOpenModalHandler = () => {
+    setOpenModal(true);
+  };
+
+  const getStringAbbreviation = (str: string, size: number) => {
+    const max = 10;
+    const offset = max / size;
+    return str.substring(0, offset);
+  };
+
+  const getAbbreviation = (name: string) => {
+    let final = '';
+    const splited = name.split(' ');
+    const size = splited.length;
+
+    splited.forEach((str: string) => {
+      final += getStringAbbreviation(str, size);
+    });
+
+    return final.substring(0, 10).toUpperCase().replace('-', '');
+  };
+
+  const onSaveHandler = () => {
+    const alias = getAbbreviation(projectTitle);
+    console.log('ALIAS:: ', alias);
+    clearFields();
+  };
+
   return (
     <>
       <Fab
@@ -27,6 +71,7 @@ const Home: React.FC = () => {
         aria-label="add"
         title="Novo projeto"
         sx={{ float: 'right' }}
+        onClick={onOpenModalHandler}
       >
         <AddIcon />
       </Fab>
@@ -36,6 +81,28 @@ const Home: React.FC = () => {
         <ProjectList label="Meus projetos" projects={myProjects} />
         <ProjectList label="Projeto públicos" projects={publicProjects} />
       </HomeContainer>
+
+      <Modal
+        maxWidth="md"
+        open={openModal}
+        title="Novo projeto"
+        onClose={onCloseModalHandler}
+        onOk={onSaveHandler}
+        disableBackDropClose={true}
+        disableEscClose={true}
+      >
+        <Container sx={{ padding: '2rem', width: '60rem' }}>
+          <Box sx={{ display: 'grid', gridTemplateColumns: '1fr max-content', gap: 1 }}>
+            <Input label="Título" onChange={(text) => setProjectTitle(text)} value={projectTitle} />
+            <Input label="Alias" onChange={(text) => setProjectTitle(text)} value={projectTitle} />
+          </Box>
+          <TextAreaInput
+            label="Descrição"
+            onChange={(text) => setProjectDescription(text)}
+            value={projectDescription}
+          />
+        </Container>
+      </Modal>
     </>
   );
 };
